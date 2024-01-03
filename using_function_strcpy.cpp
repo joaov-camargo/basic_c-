2639b27377c4334114267340a283e48504b209b4
joaoc <joaoc@comprovei.com>
2024-01-03 13:25:14 -0300

    Learning more about how using function strcpy in c++

---
+++ using_function_strcpy.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+main(){
+    char linguagem[20];
+    
+    strcpy(linguagem, "Abcdef");
+    
+
+    
+    printf("Escrito: %s", linguagem);
+}
